25c8038c669a3002d72a61f1a9e334a303b4bd54
Jason Edokpa <jasonedokpa@gmail.com>
2022-08-19 00:02:07 -0500

    offerTypeSection component created

--- force-app/main/default/lwc/customerOfferMainPage/customerOfferMainPage.js
+++ force-app/main/default/lwc/customerOfferMainPage/customerOfferMainPage.js
@@ -3,11 +3,32 @@ import getOffers from '@salesforce/apex/customerOpportunity.getOffers';
 
 export default class MainPage extends LightningElement
 {
+	
 	@track allOffers = []
 
 	@wire (getOffers)
 	offersList(result){
 		this.allOffers = result.data
-		console.log(result.data)
+		console.log('HELLO RESULTS:', result.data)
+	}
+
+	renderedCallback()
+	{
+		console.log('RENDERED CALLBACK: ')
+		console.log(this.groupByOffer(this.allOffers))	
+	}
+
+	groupByOffer(offersArray)
+	{
+		let  returnObject = {}
+
+		for (const offer of offersArray)
+		{
+			if (!(offer.McaApp__Deal_Type__c in returnObject))
+				returnObject[offer.McaApp__Deal_Type__c] = []
+			returnObject[offer.McaApp__Deal_Type__c].push(offer)
+		}
+
+		return returnObject
 	}
 }
