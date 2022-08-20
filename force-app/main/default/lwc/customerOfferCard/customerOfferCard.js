33c620116c042e8c656a343dd06a0e62b79664f4
Jason Edokpa <jasonedokpa@gmail.com>
2022-08-20 02:10:47 -0500

    records properly showing fields

--- force-app/main/default/lwc/customerOfferCard/customerOfferCard.js
+++ force-app/main/default/lwc/customerOfferCard/customerOfferCard.js
@@ -1,6 +1,31 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class CustomerOfferCard extends LightningElement 
 {
-	
+	@api offerObject = {"Id": 'null'}
+
+	minFundingAmt;
+	maxFundingAmt;
+	paybackAmt;
+	termInMonths;
+	pmtSchedule;
+	paymentAmt;
+	prepayOptions;
+	closingDocumentsRequired;
+
+	connectedCallback()
+	{
+		if (this.offerObject)
+		{
+			this.paybackAmt = this.offerObject.McaApp__Payment_Amt__c;
+			this.termInMonths = this.offerObject.McaApp__Term_Months__c;
+			this.pmtSchedule = this.offerObject.McaApp__PMT_Schedule__c;
+			this.paymentAmt = this.offerObject.McaApp__Payment_Amt__c;
+		}
+	}
+
+	handleClick(event)
+	{
+		console.log("CLICKED ON:", JSON.stringify(event.target.id).slice(1,-4) + " offer")
+	}
 }
--- force-app/main/default/lwc/offerTypeSection/offerTypeSection.js
+++ force-app/main/default/lwc/offerTypeSection/offerTypeSection.js
@@ -1,6 +1,7 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class OfferTypeSection extends LightningElement 
 {
-	
+	@api offerList = [];
+	@api groupName;
 }
