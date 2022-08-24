24e6b4deef290022c56027cd8bd18d3beb2e6630
Jason Edokpa <jasonedokpa@gmail.com>
2022-08-24 10:58:42 -0500

    added slider functionality and offerSelectModal

--- force-app/main/default/lwc/customerOfferCard/customerOfferCard.js
+++ force-app/main/default/lwc/customerOfferCard/customerOfferCard.js
@@ -5,8 +5,11 @@ export default class CustomerOfferCard extends LightningElement
 {
 	@api offerObject = {"Id": "null", "McaApp__Term_Months__c": "[Term_In_Months]"};
 
+	showModal = false;
+
 	minFundingAmt;
 	maxFundingAmt;
+	fundingAmt;
 	paybackAmt;
 	termInMonths;
 	pmtSchedule;
@@ -18,6 +21,9 @@ export default class CustomerOfferCard extends LightningElement
 	{
 		if (this.offerObject)
 		{
+			this.minFundingAmt = this.offerObject.Min_Funding_Amount__c;
+			this.maxFundingAmt = this.offerObject.Max_Funding_Amount__c;
+			this.fundingAmt = this.offerObject.McaApp__Amount__c;
 			this.paybackAmt = this.offerObject.McaApp__Payment_Amt__c;
 			this.termInMonths = this.offerObject.McaApp__Term_Months__c;
 			this.pmtSchedule = this.offerObject.McaApp__PMT_Schedule__c;
@@ -25,10 +31,16 @@ export default class CustomerOfferCard extends LightningElement
 		}
 	}
 
-	handleClick()
+	changeFundingAmt(event)
 	{
-		console.log("CLICKED ON:", this.offerObject.Name + " offer");
-		setChosenOffer({OfferID: this.offerObject.Id
+		this.fundingAmt = event.target.value;
+	}
+
+	selectThisOffer()
+	{
+		console.log("CLICKED ON: " + this.offerObject.Name + " offer");
+		console.log("Funding Amount: " + this.fundingAmt);
+		setChosenOffer({OfferID: this.offerObject.Id, newFundingAmount: this.fundingAmt
 		}).then(response => {
 			console.log(response)
 			}).catch(err => {
