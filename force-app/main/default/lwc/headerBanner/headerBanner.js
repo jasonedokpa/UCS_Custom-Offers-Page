984b8ea15d279065a91d5e5479857f6ab8ffe7f9
Jason Edokpa <jasonedokpa@gmail.com>
2022-10-18 08:22:42 -0500

    headerBanner component changes

--- force-app/main/default/lwc/headerBanner/headerBanner.js
+++ force-app/main/default/lwc/headerBanner/headerBanner.js
@@ -2,9 +2,15 @@ import { LightningElement } from 'lwc';
 import LOGOImg from '@salesforce/resourceUrl/UCSPortalLogo';
 import RatingImg from '@salesforce/resourceUrl/UCSPortalRating';
 import PhoneIcon from '@salesforce/resourceUrl/UCSPortalPhoneIcon';
+// import backgroundUrl from '@salesforce/resourceUrl/UCSDesktopBG';
+// import backgroundUrlMob from '@salesforce/resourceUrl/UCSMobileBG';
 
 export default class HeaderBanner extends LightningElement {
     UCSLogoImg = LOGOImg;
     UCSRating = RatingImg;
     UCSPhoneIcon = PhoneIcon;
+
+    // get backgroundStyle() {
+    //     return `height:50rem;background-image:url(${backgroundUrl})`;
+    // }
 }
