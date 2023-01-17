f903d38b4ca40d2cf756322793142aebc2024851
Jason Edokpa <jasonedokpa@gmail.com>
2023-01-17 09:14:26 -0600

    Anant's uncomitted changes

--- force-app/main/default/lwc/customFileUpload/customFileUpload.js
+++ force-app/main/default/lwc/customFileUpload/customFileUpload.js
@@ -1,6 +1,7 @@
 import {LightningElement,api,track} from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import uploadFiles from '@salesforce/apex/FileUploaderClass.uploadFiles'
+import deleteFiles from '@salesforce/apex/FileUploaderClass.deleteFiles'
 import { loadStyle } from 'lightning/platformResourceLoader';
 import fileSelectorStyle from '@salesforce/resourceUrl/fileSelectorStyle';
 import FORM_FACTOR from '@salesforce/client/formFactor'
@@ -9,6 +10,8 @@ const MAX_FILE_SIZE = 2097152;
 export default class CustomFileUpload extends LightningElement {
     @api recordId;
     @track filesData = [];
+    @track filesData1=[];
+    showFileNames=false;
     @api mobileRender;
     showSpinner = false;
     
@@ -71,7 +74,19 @@ export default class CustomFileUpload extends LightningElement {
  
     removeReceiptImage(event) {
         var index = event.currentTarget.dataset.id;
-        this.filesData.splice(index, 1);
+        console.log(event.currentTarget.dataset.item);
+        deleteFiles({sdocumentId : event.currentTarget.dataset.item}).then(result => {
+            console.log(result);
+            if(result && result === 'success') {
+                this.filesData1.splice(index, 1);
+            } else {
+                this.showToast('Error', 'error', result);
+            }
+        }).catch(error => {
+            if(error && error.body && error.body.message) {
+                this.showToast('Error', 'error', error.body.message);
+            }
+        })
     }
  
     showToast(title, variant, message) {
@@ -82,5 +97,27 @@ export default class CustomFileUpload extends LightningElement {
                 message: message,
             })
         );
+    }
+
+    get acceptedFormats() {
+        return ['.xlsx, .xls, .csv, .png, .doc, .docx, .pdf'];
+    }
+
+    handleUploadFinished(event) {
+        const uploadedFiles = event.detail.files;
+        console.log(event.detail.files);
+        // Get the list of uploaded files
+        for(var i=0; i< uploadedFiles.length; i++){
+            this.showFileNames = true;
+            let fileDetails = {};
+            console.log('uploadedFiles[0].name',uploadedFiles[i].name);
+            fileDetails.Name = uploadedFiles[i].name;
+            console.log('uploadedFiles[0].documentId',uploadedFiles[i].documentId);
+            fileDetails.documentId = uploadedFiles[i].documentId;
+            fileDetails.Number = i;
+            this.filesData1[i] = fileDetails;
+        }
+        
+        console.log('filesData1 ', this.filesData1);
     }
 }
